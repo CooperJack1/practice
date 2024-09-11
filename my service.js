0431893367b697f305675df2884089b548ee995a
cooper <1633016740@qq.com>
2024-09-11 17:12:20 +0800

    11111

--- my service.js
+++
@@ -1,7 +0,0 @@
-const http= require('http');
-http.createServer((req, res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain; charset=UTF-8'});
-    res.end('hello cooper!');
-}).listen(3000);
-
-console.log('server run on 3000 port');
---
+++ proxy_service.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+// 创建服务器
+const app = express();
+app.get('/hero', async (req, res) => {
+    res.header('access-control-allow-origin', '*');
+    const result = await axios.get('https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js');
+    // res.send('你好 我是你忠实的代理')
+    res.send(result.data)
+});
+app.listen(9527, () => {
+    console.log('服务器已启动');
+});
