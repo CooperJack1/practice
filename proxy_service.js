2ada232003c45367b7b5d60f7b1be116b43c0d2b
cooper <1633016740@qq.com>
2024-11-11 14:27:37 +0800

    11111

--- proxy_service.js
+++ proxy_service.js
@@ -6,7 +6,8 @@ app.get('/hero', async (req, res) => {
     res.header('access-control-allow-origin', '*');
     const result = await axios.get('https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js');
     // res.send('你好 我是你忠实的代理')
-    res.send(result.data)
+    // res.json(result.data);
+    res.json(JSON.parse(JSON.stringify({name:'cooper'})) );
 });
 app.listen(9527, () => {
     console.log('服务器已启动');
